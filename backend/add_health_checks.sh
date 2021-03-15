1fc83a7d8c4c7a573a28839b434f62951862b268
MGL <glmitin@gmail.com>
2021-03-15 22:31:40 +0300

    Small corrections

--- backend/add_health_checks.sh
+++ backend/add_health_checks.sh
@@ -2,5 +2,5 @@
 
 APP_NAME=backend
 
-oc set probe dc/${APP_NAME} --readiness --period-seconds=1 --success-threshold=1 --timeout-seconds=1 --failure-threshold=2 --get-url=http://:8080/health/readiness
-oc set probe dc/${APP_NAME} --liveness --period-seconds=5 --success-threshold=1 --timeout-seconds=1 --failure-threshold=2 --get-url=http://:8080/health/liveness
+oc set probe dc/${APP_NAME} --readiness --initial-delay-seconds=30 --period-seconds=1 --success-threshold=1 --timeout-seconds=1 --failure-threshold=2 --get-url=http://:8080/health/readiness
+oc set probe dc/${APP_NAME} --liveness --initial-delay-seconds=30 --period-seconds=5 --success-threshold=1 --timeout-seconds=1 --failure-threshold=2 --get-url=http://:8080/health/liveness
